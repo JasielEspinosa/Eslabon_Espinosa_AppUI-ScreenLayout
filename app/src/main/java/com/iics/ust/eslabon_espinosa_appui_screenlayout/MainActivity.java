a35623163dece3935dfd4078e02154033fa97d27
Jasiel Espinosa <jaszespi@gmail.com>
2017-09-24 22:24:43 +0800

    Reset and Clean

--- app/src/main/java/com/iics/ust/eslabon_espinosa_appui_screenlayout/MainActivity.java
+++ app/src/main/java/com/iics/ust/eslabon_espinosa_appui_screenlayout/MainActivity.java
@@ -1,15 +1,17 @@
 package com.iics.ust.eslabon_espinosa_appui_screenlayout;
 
+
+import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +28,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (drawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return drawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 }
